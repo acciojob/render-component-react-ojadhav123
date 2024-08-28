67601b5d077c5beb01f28654b9990a456e689b0e
ojadhav123 <omkarjadhav787@gmail.com>
2024-08-28 21:32:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => {
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <div>
+      <h1>My React App</h1>
+      <p>I am learning React. My life is getting better.</p>
+      <ButtonCounter />
+    </div>
+  );
 }
 
+function ButtonCounter() {
+  const [count, setCount] = useState(0);
 
-export default App;
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me!</button>
+      <p>Button clicked {count} times.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
